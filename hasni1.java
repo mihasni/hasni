cc84a531d88d68013ba75a57de97c62eae86bf15
MOHD IMRAN <jahuddin@Jahuddins-MacBook-Air.local>
2023-08-13 16:25:01 +0530

    TO CALCULATE AVERAGE MARKS

---
+++ hasni1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class hasni1{
+ public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        String str=s.next();
+        int m1=s.nextInt();
+        int m2=s.nextInt();
+        int m3=s.nextInt();
+        s.close();
+        System.out.println(str);
+        System.out.println(((m1+m2+m3)/3));
+    }}
