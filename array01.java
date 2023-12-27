e0972dad8e6effb826f374fad7898414e5c2aa55
mihasni <142148662+mihasni@users.noreply.github.com>
2023-12-27 13:34:30 +0530

    array basics program

---
+++ array01.java
@@ -0,0 +1,9 @@
+public class array01 {
+    public static void main(String[] args) {
+        int arr[]=new int[10];
+        arr[0]=5;
+        arr[5]=17;
+        System.out.println(arr[0]);
+    }
+    
+}
